923251d3e60a38bbe9b8a411fec2ba7e9e948cbf
Dakofer <fer.hernandez.amezola@gmail.com>
2022-09-04 13:36:09 -0700

    Se ha agregado la conexion ala base de datos para que se habra y se cierre

---
+++ Datos/D_Usuarios.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Datos
+{
+    public class D_Usuarios
+    {
+        public SqlConnection Conexion;
+
+        public D_Usuarios(SqlConnection Conexion)
+            {
+            Conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["conexionBD"].ConnectionString);
+            }
+
+        public SqlConnection GetConexion()
+        {
+            return Conexion;
+        }
+
+        public void AbrirConexion(SqlConnection Conexion)
+        {
+            try
+            {
+                if (Conexion.State == ConnectionState.Broken || Conexion.State == ConnectionState.Closed)
+                {
+                    Conexion.Open();
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al tratar de conectar",ex);
+            }
+        }
+
+        public void CerrarConexion(SqlConnection Conexion)
+        {
+            try
+            {
+                if (Conexion.State == ConnectionState.Open)
+                {
+                    Conexion.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al tratar de Cerrar la conexion", ex);
+            }
+        }
+    }
+}
