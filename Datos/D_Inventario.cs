be335dcbe1294cdc3bf4fdfb0fd06bee44195319
Dakofer <fer.hernandez.amezola@gmail.com>
2022-09-18 22:29:32 -0700

    Arregle todo el cagadero

---
+++ Datos/D_Inventario.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data;
+using System.Data.SqlClient;
+
+using Entidades;
+
+namespace Datos
+{
+    public class D_Inventario: D_Conexion
+    {
+        public D_Inventario() { }
+
+        public int IBMInventario(string pAccion, E_Inventario objeE_Inventario)
+        {
+            int Resultado = 0;
+
+            SqlCommand cmd = new SqlCommand("IBM_Inventario", Conexion)//si esta mal es por que el nombre del stored procedure esta mal
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            cmd.Parameters.AddWithValue("@Accion", pAccion);
+            cmd.Parameters.AddWithValue("@IdInventario", objeE_Inventario.IdInventario);
+            cmd.Parameters.AddWithValue("@IdArticulo", objeE_Inventario.IdArticulo);
+            cmd.Parameters.AddWithValue("@NombreArticulo", objeE_Inventario.NombreArticulo);
+            cmd.Parameters.AddWithValue("@Descripcion", objeE_Inventario.Descripcion);
+            cmd.Parameters.AddWithValue("@ClaveLaboratorio", objeE_Inventario.ClaveLaboratorio);
+            cmd.Parameters.AddWithValue("@Cantidad", objeE_Inventario.Cantidad);
+
+            try
+            {
+                AbrirConexion();
+                Resultado = cmd.ExecuteNonQuery();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    Resultado = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ERROR al tratar de almacenar, modificar o borrar", ex);
+            }
+            finally
+            {
+                CerrarConexion();
+                cmd.Dispose();
+            }
+            return Resultado;
+        }   
+    }
+}
---
+++ Entidades/E_Inventario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Entidades
+{
+    public class E_Inventario
+    {
+        #region 
+        private int _IdInventario;
+        private int _IdArticulo;
+        private string _NombreArticulo;
+        private string _Descripcion;
+        private int _ClaveLaboratorio;
+        private int _Cantidad;
+        #endregion
+
+        public E_Inventario(int idInventario, int idArticulo, string nombreArticulo, string descripcion, int claveLaboratorio, int cantidad)
+        {
+            IdInventario = idInventario;
+            IdArticulo = idArticulo;
+            NombreArticulo = nombreArticulo;
+            Descripcion = descripcion;
+            ClaveLaboratorio = claveLaboratorio;
+            Cantidad = cantidad;
+        }
+
+        public int IdInventario { get => _IdInventario; set => _IdInventario = value; }
+        public int IdArticulo { get => _IdArticulo; set => _IdArticulo = value; }
+        public string NombreArticulo { get => _NombreArticulo; set => _NombreArticulo = value; }
+        public string Descripcion { get => _Descripcion; set => _Descripcion = value; }
+        public int ClaveLaboratorio { get => _ClaveLaboratorio; set => _ClaveLaboratorio = value; }
+        public int Cantidad { get => _Cantidad; set => _Cantidad = value; }
+    }
+}
